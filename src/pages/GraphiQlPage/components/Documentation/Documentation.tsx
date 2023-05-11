17b4623e024a8ca246da405de07581e2c96f25d8
Sergei Buiko <sergeibuiko1@gmail.com>
2023-05-11 16:21:12 +0300

    init: start documentation feature

--- src/pages/GraphiQlPage/components/Documentation/Documentation.tsx
+++ src/pages/GraphiQlPage/components/Documentation/Documentation.tsx
@@ -1,3 +1,30 @@
 export function Documentation() {
-  return <div>Documentation</div>;
+  const showGraphQLData = () => {
+    const query = `
+    query {
+      character (id: 2) {
+       id
+       name
+       status
+      }
+    }`;
+
+    fetch('https://rickandmortyapi.graphcdn.app/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({ query }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => console.log(data));
+  };
+  return (
+    <div>
+      <button onClick={showGraphQLData}>Show Shema</button>
+    </div>
+  );
 }
