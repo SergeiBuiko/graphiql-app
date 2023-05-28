c5c85d1967c16cd73f85079b2703baacf6d3be6d
Sergei Buiko <106577841+SergeiBuiko@users.noreply.github.com>
2023-05-28 13:22:57 +0300

    Update src/pages/GraphiQlPage/components/Documentation/ElementParams/ElementParams.tsx
    Co-authored-by: xobby-tt <nastya-nastenkaa@mail.ru>

--- src/pages/GraphiQlPage/components/Documentation/ElementParams/ElementParams.tsx
+++ src/pages/GraphiQlPage/components/Documentation/ElementParams/ElementParams.tsx
@@ -11,36 +11,27 @@ export function ElementParams({ el, addParams }: IParamsProps) {
   return (
     <span>
       (
-      {el.args?.map((elem: any, id: number) => (
-        <span key={id}>
-          <span style={{ color: 'red' }}>{elem.name}</span>:{' '}
-          <Link
-            style={{ color: 'orange' }}
-            underline="hover"
-            href="#"
-            onClick={(event) => {
-              event.preventDefault();
-              addParams(
-                elem.type?.name
-                  ? elem.type?.name
-                  : elem.type?.ofType?.name
-                  ? elem.type?.ofType?.name
-                  : elem.type?.ofType?.ofType?.name
-                  ? elem.type?.ofType?.ofType?.name
-                  : elem.type?.ofType?.ofType?.ofType?.name
-              );
-            }}
-          >
-            {elem.type?.name
-              ? elem.type?.name
-              : elem.type?.ofType?.name
-              ? elem.type?.ofType?.name
-              : elem.type?.ofType?.ofType?.name
-              ? elem.type?.ofType?.ofType?.name
-              : elem.type?.ofType?.ofType?.ofType?.name}
-          </Link>{' '}
-        </span>
-      ))}{' '}
+  {el.args?.map((elem: any, id: number) => {
+        const elementName = elem.type?.name || getElementOfTypeName(elem.type);
+
+        return (
+          <span key={id}>
+            <span style={{ color: 'red' }}>{elem.name}</span>:{' '}
+            <Link
+              style={{ color: 'orange' }}
+              underline="hover"
+              href="#"
+              onClick={(event) => {
+                event.preventDefault();
+
+                addParams(elementName);
+              }}
+            >
+              {elementName}
+            </Link>{' '}
+          </span>
+        );
+      })}{' '}
       )
     </span>
   );
